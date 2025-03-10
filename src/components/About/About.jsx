6d9d4ecbf8e80f037c88cc52a0498342008620e1
Laura Luque <lauraluque1215@gmail.com>
2025-03-09 19:24:14 -0500

    feat: :lipstick: add highlight2 tittle about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import styles from './About.module.css';
+import highlight from '../../assets/shapes/highlight2.svg';
 
 const About = () => {
   return (
     <section className={styles.about}>
-      <div className='about-left'>
-        <div>
-          <h1>Deliver a high-quality customer experience</h1>
+      <div>
+        <div className={styles.aboutleft}>
+          <div className={styles.titleWrapper}>
+            <img src={highlight} alt="highlight" className={styles.highlight2} />
+            <h1 className={styles.textpresent}>Deliver a high-<br />quality customer experience</h1>
+          </div>
+          <p className={styles.aboutDescription}>
+            Embrace AI chatbot technology in a GPT-4-supported <br />
+            communication tool to help your customers, empower your <br />
+            team, and reduce your workload.
+          </p>
         </div>
-        <div></div>
-
-      </div>
-      <div >
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
       </div>
 
-
+      <div className={styles.containerGridAbout}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export { About };
